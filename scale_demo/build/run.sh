ac5301dd5b13d6b0f017824961eaaa0d0b562073
Joseph Hunt <josephhunt@Josephs-MacBook-Pro.local>
2017-08-11 12:03:58 -0500

    stubbed out FQ file paths for mount points

--- scale_demo/build/run.sh
+++
@@ -1,9 +0,0 @@
-# sudo not required for mac, but is for linux
-DOCKER="docker"
-if [[ "$(uname -s)" == "Linux" ]]; then
-        DOCKER="sudo docker"
-fi
-
-# runs built container image and logs into w/ a bash shell for debugging
-$DOCKER run -dit --name foobar cdemo/curl
-$DOCKER exec -it foobar /bin/bash
