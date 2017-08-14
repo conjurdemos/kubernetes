a1e21c30bbd912611322e42a69edc16236a40881
Joseph Hunt <josephhunt@Josephs-MacBook-Pro.local>
2017-08-13 22:24:29 -0500

    delete dreck

--- haproxy/container-haproxy-https-master/one_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-eval $(docker-machine env default)
-docker ps -a | grep Exited | awk '{print $1}' | xargs -I {} docker start {}
--- haproxy/container-haproxy-https-master/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Targeting the following nodes:"
-docker ps -f name=haproxy-https-
-
-echo ""
-echo "Stopping now..."
-docker ps -qa -f name=haproxy-https- | xargs docker rm -f
-echo "Done."
