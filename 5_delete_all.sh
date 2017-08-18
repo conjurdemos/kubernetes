a928dec46c341c0fa1f53db10e6dbb095a9aa84b
Kevin Gilpin <kgilpin@gmail.com>
2017-08-18 16:38:49 -0400

    delete more stuff

--- 5_delete_all.sh
+++ 5_delete_all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 kubectl delete -f ./conjur-service
+kubectl delete replicaset -lapp=webapp
 kubectl delete replicaset -lapp=conjur-master
 kubectl delete replicaset -lapp=conjur-follower
 kubectl delete pods -lapp=conjur-appliance
-kubectl delete pods webapp
+kubectl delete pods -lapp=webapp
 kubectl delete pods haproxy-conjur-master
 rm ./conjur-service/*.tar
