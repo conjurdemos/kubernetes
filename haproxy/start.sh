5c6f3f05eca48a88ca8f192e3b267d0dc6bd87a8
Kevin Gilpin <kgilpin@gmail.com>
2017-09-05 14:23:02 -0400

    turn off debug mode

--- haproxy/start.sh
+++ haproxy/start.sh
@@ -5,4 +5,4 @@ cd /usr/local/etc/haproxy/
 /root/pg_servers.sh
 /root/http_servers.sh
 
-exec haproxy -d -f ./haproxy.cfg -f ./http_servers.cfg -f ./pg_servers.cfg
+exec haproxy -f ./haproxy.cfg -f ./http_servers.cfg -f ./pg_servers.cfg
