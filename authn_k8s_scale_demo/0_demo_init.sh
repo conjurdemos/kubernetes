b354cffe39a79658afe6e24827690c62f0b69d2a
Kevin Gilpin <kgilpin@gmail.com>
2017-09-05 14:24:10 -0400

    install the 'policy' plugin

--- authn_k8s_scale_demo/0_demo_init.sh
+++ authn_k8s_scale_demo/0_demo_init.sh
@@ -3,9 +3,9 @@ set -o pipefail
 conjur init -h conjur-master -f conjurrc
 export CONJURRC=$(pwd)/conjurrc
 printf "Login with password 'Cyberark1'...\n"
+conjur plugin install policy
 conjur authn login admin
 conjur bootstrap
 cd build
 ./build.sh
 cd ..
-
