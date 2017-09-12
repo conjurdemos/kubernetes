ae1e4e4066f2f1ce4e6ac4f6ebdb1f70627deb72
Kevin Gilpin <kgilpin@gmail.com>
2017-09-12 13:19:32 -0400

    delete more stuff

--- 5_delete_all.sh
+++ 5_delete_all.sh
@@ -2,9 +2,12 @@
 
 declare CONJUR_NAMESPACE=conjur
 
-kubectl delete -f ./authn_k8s_scale_demo/webapp.yaml
+kubectl delete --ignore-not-found=true -f ./authn_k8s_scale_demo/webapp.yaml
 kubectl delete replicaset -lapp=webapp
 kubectl delete pods -lapp=webapp
+
+kubectl delete pods --ignore-not-found=true conjur-cli
+kubectl delete --ignore-not-found=true configmap cli-conjur
 
 kubectl delete namespace $CONJUR_NAMESPACE
 
