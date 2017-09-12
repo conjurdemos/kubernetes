3270695e4831db7c1983c873773b6bcec1f3d644
Kevin Gilpin <kgilpin@gmail.com>
2017-09-12 14:19:23 -0400

    tweak the login prompt

--- authn_k8s_scale_demo/0_demo_init.sh
+++ authn_k8s_scale_demo/0_demo_init.sh
@@ -6,7 +6,7 @@ kubectl config use-context minikube
 conjur init -h conjur-master -f conjurrc
 export CONJURRC=$(pwd)/conjurrc
 conjur plugin install policy
-printf "When prompted, login with password 'Cyberark1':\n"
+printf "When prompted, login with password 'Cyberark1'\n"
 conjur authn login admin
 conjur bootstrap
 cd build
