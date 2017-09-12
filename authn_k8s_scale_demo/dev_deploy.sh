32fc40d7ced611a20f4b35715ff86f0ade40d790
Kevin Gilpin <kgilpin@gmail.com>
2017-09-12 14:31:57 -0400

    summon wip

--- authn_k8s_scale_demo/dev_deploy.sh
+++ authn_k8s_scale_demo/dev_deploy.sh
@@ -2,19 +2,24 @@
 
 source ./evokecmd.sh
 
+kubectl config use-context conjur
+
 echo Grabbing the conjur.pem
 ssl_certificate=$(evokecmd cat /opt/conjur/etc/ssl/ca.pem)
 
+kubectl config use-context minikube
+
 echo Storing non-secret configuration data
 
-# write out host factory name & token, variable name and deployment name in configmap
+# write out conjur ssl cert in configmap
 kubectl delete configmap webapp
 kubectl create configmap webapp \
   --from-literal=ssl_certificate="$ssl_certificate"
 
 export CLIENT_API_KEY=$(conjur host rotate_api_key -h conjur/authn-k8s/minikube/default/client)
-echo Client API key: $CLIENT_API_KEY
+echo Environment token: $CLIENT_API_KEY
 
+# save client API key as k8s secret
 kubectl delete secret conjur-client-api-key
 kubectl create secret generic conjur-client-api-key --from-literal "api-key=$CLIENT_API_KEY"
 
